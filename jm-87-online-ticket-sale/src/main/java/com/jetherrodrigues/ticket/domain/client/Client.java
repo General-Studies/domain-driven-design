f0fe1cecad3d302d2b96387bd30ee07016f12d75
Jether Rodrigues <jether.rodrigues@geovoxel.com>
2020-07-30 15:58:15 -0300

    updated der

---
+++ jm-87-online-ticket-sale/src/main/java/com/jetherrodrigues/ticket/domain/client/Client.java
@@ -0,0 +1,2 @@
+package com.jetherrodrigues.ticket.domain.client;public class Client {
+}
---
+++ jm-87-online-ticket-sale/src/main/java/com/jetherrodrigues/ticket/domain/order/types/CreditCardOperatorType.java
@@ -0,0 +1,2 @@
+package com.jetherrodrigues.ticket.domain.order.types;public enum CreditCardOperatorType {
+}
