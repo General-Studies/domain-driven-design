cd4de2092542608c4d6c9f968439091821ac0a94
Jether Rodrigues <jether.rodrigues@geovoxel.com>
2020-07-30 14:26:11 -0300

    created initial structure of the jm 87 to pratice ddd

---
+++ jm-87-online-ticket-sale/src/main/java/com/jetherrodrigues/ticket/Jm87OnlineTicketSaleApplication.java
@@ -0,0 +1,13 @@
+package com.jetherrodrigues.ticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jm87OnlineTicketSaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jm87OnlineTicketSaleApplication.class, args);
+	}
+
+}
---
+++ jm-87-online-ticket-sale/src/test/java/com/jetherrodrigues/ticket/Jm87OnlineTicketSaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jetherrodrigues.ticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jm87OnlineTicketSaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
