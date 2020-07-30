f33661d59153a87ef18f8fec5b41b4fa8671b30b
Jether Rodrigues <jether.rodrigues@geovoxel.com>
2020-07-30 16:33:52 -0300

    renamed application main class

--- jm-87-online-ticket-sale/src/main/java/com/jetherrodrigues/ticket/OnlineTicketSaleApplication.java
+++ jm-87-online-ticket-sale/src/main/java/com/jetherrodrigues/ticket/OnlineTicketSaleApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Jm87OnlineTicketSaleApplication {
+public class OnlineTicketSaleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Jm87OnlineTicketSaleApplication.class, args);
+		SpringApplication.run(OnlineTicketSaleApplication.class, args);
 	}
 
 }
