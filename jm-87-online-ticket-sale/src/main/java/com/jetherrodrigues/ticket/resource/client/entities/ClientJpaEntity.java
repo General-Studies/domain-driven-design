6c272317597d272a6663f6f77032a154c8315047
Jether Rodrigues <jether.rodrigues@geovoxel.com>
2020-07-30 16:34:32 -0300

    created jpa client entity

--- jm-87-online-ticket-sale/src/main/java/com/jetherrodrigues/ticket/resource/client/entities/ClientJpaEntity.java
+++ jm-87-online-ticket-sale/src/main/java/com/jetherrodrigues/ticket/resource/client/entities/ClientJpaEntity.java
@@ -1,19 +1,26 @@
 package com.jetherrodrigues.ticket.resource.client.entities;
 
+import com.jetherrodrigues.ticket.domain.client.entities.Client;
 import io.azam.ulidj.ULID;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.io.Serializable;
 
 @Entity
-public final class Client implements Serializable {
+public final class ClientJpaEntity implements Serializable {
+    @Id
     private final String id;
 
-    public Client(String id) {
+    public ClientJpaEntity() {
         this.id = ULID.random();
     }
 
     public String getId() {
         return id;
+    }
+
+    public static ClientJpaEntity from(final Client client) {
+        return new ClientJpaEntity();
     }
 }
